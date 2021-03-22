c5816ce274b487fecc45e32442d87351d192748d
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2021-03-21 21:24:33 -0400

    Update quicsortprrintV2.asm

--- src/algorithms/quicksort/quicsortprrintV2.asm
+++ src/algorithms/quicksort/quicsortprrintV2.asm
@@ -2,13 +2,16 @@
 
 # The SLT instruction sets the destination register's content to the value 1 if the first source register's contents are 
 # less than the second source register's contents. Otherwise, it is set to the value 0.
-
-
-
 # It's syntax is: 
 # SLT $destination register's address, $first source register's address, $second source register's address.
 
 # The sample SLT instruction demonstrated in the datapath above is SLT $17, $19, $22.
+
+# The ADDI instruction performs an addition on both the source register's contents and the immediate data, 
+# and stores the result in the destination register.
+
+It's syntax is: 
+ADDI $destination register's address, $source register's address, immediate data.
 
 
 ################################################################################################
