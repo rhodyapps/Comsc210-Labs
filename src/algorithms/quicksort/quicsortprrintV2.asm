4a86589e6cbe1501213401a5b1f55ea0a40c70aa
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2021-03-21 20:44:04 -0400

    Create quicsortprrintV2.asm
    Add documentation

---
+++ src/algorithms/quicksort/quicsortprrintV2.asm
@@ -0,0 +1,11 @@
+# QUICKSORT with print method
+
+# The SLT instruction sets the destination register's content to the value 1 if the first source register's contents are 
+# less than the second source register's contents. Otherwise, it is set to the value 0.
+
+
+
+# It's syntax is: 
+# SLT $destination register's address, $first source register's address, $second source register's address.
+
+The sample SLT instruction demonstrated in the datapath above is SLT $17, $19, $22.
