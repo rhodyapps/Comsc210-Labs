6e52d38e529c671a256eeedf275098a66e686c9f
Tom Newman <rhodyapps@gmail.com>
2022-01-19 15:00:05 -0500

    lab 1 update

--- src/Lab1_HelloWorld.asm
+++ src/Lab1_HelloWorld.asm
@@ -8,14 +8,25 @@ Program 1: Hello World
 
 .data
 
-# This is the section of the program where we establish data components such
+# Every assembly language program will have two sections: .data and .text
+# the '.data' section is the section of the program where we tell the compiler where to find our data components such
 # as prompts and responses to be displayed on the console.
 
+# In this program we will have one data component to hold the ascii string 'hello world'
+# Note that the way we declare a variable (or constant) is with a label such as the following:
+
 message: .asciiz "hello world\n" # Here is the message we will display
+
+# Next we have our '.text' section where we will tell the compiler which instructions to follow.
+
 .text
 
-# This is the section of the program containing our executable code
-# declare a procedure main that can be referenced from other files (global)
+# The '.text' section is the section of the program containing our executable code
+# Assembler language instructions are executed one at a time, following the sequence that
+# we have used in our program. 
+
+# The next statement is a directive '.globl' that tells the compiler the function
+that is being identified (in theis case 'main' can be referenced from other files (global)
 
 .globl main 
 
